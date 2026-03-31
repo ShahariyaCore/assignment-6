f1321680b3cda9f2b1894795b4c0a5b161ed9c38
nishat822 <shahariya.jaman00@gmail.com>
2026-04-01 00:52:12 +0600

    feat: add product listing UI
    Implemented responsive product cards using Tailwind CSS with dynamic rendering using map()

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@
 import NavBar from './component/NavBar'
 import Banner from './component/Banner'
 import Counter from './component/Counter'
+import Model from './component/Model'
+
+
+const getModels = async () => {
+  const response = await fetch('/models.json')
+  const data = await response.json()
+  return data
+}
+
+const modelPromise = getModels()
 
 function App() {
   return (
@@ -9,6 +19,8 @@ function App() {
      <NavBar />
      <Banner />
       <Counter />
+      <Model modelPromise={modelPromise} />
+      
     </>
 
   )
